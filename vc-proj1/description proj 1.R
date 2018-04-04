8cd9fb136fe0bba8b2367b98d90c552c4a531086
eppofahmi <30468477+eppofahmi@users.noreply.github.com>
2018-04-04 12:29:41 +0700

    Update description proj 1.R
    adding steps

--- vc-proj1/description proj 1.R
+++ vc-proj1/description proj 1.R
@@ -1,1 +1,8 @@
-# here we are going to map why are people intersting to participate helping ecah other
+# Our first project is to see how netizens construct publicness around donation issues. 
+# In this context, we see it from the existence of the kitabisa website and 
+# discussion of kitabisa on Twitter.
+# here are our scenario:
+# 1. Get data from Twitter (tweet mentioning @kitabisacom)
+# 2. Get information about what donation that become popular among netizens
+# 3. Go to kitabisa.com to verify the objects 
+# 4. Text analysis
