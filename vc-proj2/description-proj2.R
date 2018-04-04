1ab1e0c05fe57571d0a68667ab72048761886b6d
eppofahmi <eppofahmi@gmail.com.com>
2018-04-04 16:38:21 +0700

    change desc

--- vc-proj2/description-proj2.R
+++ vc-proj2/description-proj2.R
@@ -1,2 +1,4 @@
 # here we are going to map from our 2nd object 
-# the main quiestion is how citizens?
+# here we will use two data source
+# 1. twitter
+# 2. petition 
--- vc-proj3/tweet-wrangling.R
+++ vc-proj3/tweet-wrangling.R
@@ -45,26 +45,3 @@ data$user_all = gsub("\\s+", " ", str_trim(data$user_all)) # remove space
 
 # Saving the result for next 
 # write_tsv(data, "wr_jod.tsv")
-
-# Counting user 
-jml_user <- unlist(user$username_com)
-# calculate hashtag frequencies
-jml_user = table(jml_user)
-jml_user = as.data.frame(jml_user)
-
-# explorasition 1 - username
-# akun mana (siapa) yang paling sering ngetwit dengan tagar?
-jml_user %>%
-  filter(Freq > 100) %>%
-  ggplot(aes(x = reorder(jml_user, Freq), y = Freq)) +
-  geom_col(show.legend = FALSE) +
-  coord_flip() +
-  labs(x = NULL, 
-       y = "Frekuensi sebuah akun mentwit dengan tagar #jogjaoradidol")
-
-# the answear is @dodoputrabangsa
-# apakah ada perubahan username dari waktu ke waktu?
-# step to ansear the qustion:
-## 1. make a new data frame with date, user_all, and user_count
-## 2. group the data by year - merge rows in user_all column
-## 3. count the usernames for each year/6months
