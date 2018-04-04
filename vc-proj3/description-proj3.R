592fc6ce28fe0a2d8f1970167b0b8f848bb57b9d
eppofahmi <30468477+eppofahmi@users.noreply.github.com>
2018-04-04 16:17:35 +0700

    Update description-proj3.R
    steps description

--- vc-proj3/description-proj3.R
+++ vc-proj3/description-proj3.R
@@ -1,2 +1,6 @@
-# this is a test
-
+# Data and analysis
+# 1. Twitter data mentioning @ChangeOrg_ID
+# 2. Topics - topic modelling
+# 3. Influeuncer - network analysis
+# 4. Go to change, and take the comment from specific/popular issues
+# 5. Taking more data based on popular topics and influencer timeline
