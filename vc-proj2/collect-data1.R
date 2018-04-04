abcc5f515c40b007f1fbb188843bb5e4a78ada63
eppofahmi <eppofahmi@gmail.com.com>
2018-04-04 11:27:38 +0700

    add proj 2

---
+++ vc-proj2/collect-data1.R
@@ -0,0 +1,29 @@
+library(rvest)
+library(purrr)
+library(dplyr)
+
+## geting the data
+
+url_base <- "https://www.change.org/search?q=gojek&offset=%d"
+
+map_df(c(0,10,20,30,40), function(i) {
+  cat(".")
+  page <- read_html(sprintf(url_base, i))
+  data.frame(title = html_text(html_nodes(page, ".xs-mbs")), 
+             date = html_text(html_nodes(page, ".symbol-clock+ span")),
+             suporter = html_text(html_nodes(page, ".symbol-supporters+ span")), 
+             target = html_text(html_nodes(page, ".xs-mbn .type-s")),
+             inisiator = html_text(html_nodes(page, ".list-rule") %>% 
+                                    html_nodes("div .type-ellipsis") %>%
+                                    html_nodes(".type-ellipsis strong")),
+             link = data.frame(links = html_nodes(page, ".list-rule") %>%
+                                 html_nodes("a") %>%
+                                 html_attr("href")),
+             
+             stringsAsFactors = FALSE)
+  
+}) -> tes_chang
+
+
+## data cleaning
+
