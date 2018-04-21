188d7889c8d78759c804535c8c50b021da43e6c3
eppofahmi <eppofahmi@gmail.com.com>
2018-04-21 11:18:31 +0700

    add project description

--- vc-proj3/description-proj3.R
+++
@@ -1,6 +0,0 @@
-# Change
-# 1. Twitter data mentioning @ChangeOrg_ID
-# 2. Topics - topic modelling
-# 3. Influeuncer - network analysis
-# 4. Go to change, and take the comment from specific/popular issues
-# 5. Taking more data based on popular topics and influencer timeline
