62ec12e3e87b05f7be4538c3e3d75715e66597ba
eppofahmi <eppofahmi@gmail.com.com>
2018-04-21 10:45:33 +0700

    skrip untuk scrapping daftar petisi

---
+++ vc-proj3/05 - Petition list scrapper.R
@@ -0,0 +1,47 @@
+library(rvest)
+library(purrr)
+library(dplyr)
+
+# seacrhing urls 
+url_base <- "https://www.change.org/search?q=gojek&offset=%d"
+
+# scrappe
+map_df(c(0,10,20,30,40), function(i) {
+  cat(".")
+  page <- read_html(sprintf(url_base, i))
+  data.frame(judul = html_text(html_nodes(page, ".xs-mbs")), 
+             tanggal = html_text(html_nodes(page, ".symbol-clock+ span")),
+             pendukung = html_text(html_nodes(page, ".symbol-supporters+ span")), 
+             target = html_text(html_nodes(page, ".xs-mbn .type-s")),
+             pengusul = html_text(html_nodes(page, ".list-rule") %>% 
+                                    html_nodes("div .type-ellipsis") %>%
+                                    html_nodes(".type-ellipsis strong")),
+             link = data.frame(links = html_nodes(page, ".list-rule") %>%
+                                 html_nodes("a") %>%
+                                 html_attr("href")),
+
+             stringsAsFactors = FALSE)
+  
+}) -> tes_chang
+
+# NA handling for daerah - ".plxxs"
+
+map_df(c(0,10,20,30,40), function(i) {
+  cat(".")
+  page <- read_html(sprintf(url_base, i))
+  data.frame(title = html_text(html_nodes(page, ".xs-mbs")), 
+             date = html_text(html_nodes(page, ".symbol-clock+ span")),
+             supporter = html_text(html_nodes(page, ".symbol-supporters+ span")), 
+             target = html_text(html_nodes(page, ".xs-mbn .type-s")),
+             inisiator = html_text(html_nodes(page, ".list-rule") %>% 
+                                    html_nodes("div .type-ellipsis") %>%
+                                    html_nodes(".type-ellipsis strong")),
+             link = data.frame(links = html_nodes(page, ".list-rule") %>%
+                                 html_nodes("a") %>%
+                                 html_attr("href")),
+             daerah = html_text(html_nodes(page, ".plxxs") %>%
+                                  html_node(".plxxs")),
+
+             stringsAsFactors = FALSE)
+  
+}) -> a_tes_chang
