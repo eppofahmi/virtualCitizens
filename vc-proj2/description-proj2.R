3460668c82f1ae369553d468f32236f5f3ab454d
eppofahmi <eppofahmi@gmail.com.com>
2018-04-21 11:01:46 +0700

    del old description

--- vc-proj2/description-proj2.R
+++
@@ -1,7 +0,0 @@
-# Gojek
-# 1. Twitter data mentioning @gojekindonesia
-# 2. topics 
-# 3. network analysis
-# 4. taking more data based on influencer timeline
-# 5. taking more data based on pupular hashtag/topics
-# 6. goals1: discourse construction pattern
