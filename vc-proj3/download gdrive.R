a37df020ae8f4f036ab39992f0e64b61f81b96c8
eppofahmi <eppofahmi@gmail.com>
2018-05-03 13:49:32 +0700

    download file

--- vc-proj3/download gdrive.R
+++ vc-proj3/download gdrive.R
@@ -4,10 +4,12 @@ library(googledrive)
 library(tidyverse)
 
 id0 <- "1RAalTLYhpprDCpE_wos48ymufN8mjMG_" #twit-mention-change.csv 
-id1 <- "0B-wuZ2XMFIBUd09Ob0pKVkRzQTA" # twit-tagar-balitolakreklamasi.csv
+id1 <- "11WTedaaE79EtT4vOGNU2vfG7xgcLtJcE" # twit-tagar-balitolakreklamasi.csv
 id2 <- "1984jmFur63PmYvoC8Ab6P2iwfUQYVp71" # twit-tagar-balinotforsale.csv
 
-to_change <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+twit_to_change <- read_csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id0))
+btr_raw <- read_csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id1))
+bns_raw <- read_csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id2))
 
+glimpse(btr_raw)
 
-drive_find(n_max = 50)
